34cf7af70cfc57fdb32607cd726e0b22f94766ee
DaryllCulas <culasdaryll99@gmail.com>
2023-09-15 01:47:01 +0800

    Added and debugged Fibonacci.js

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,9 +1,8 @@
 function fibonacciSequence(n) {
   const fib = [0,1];
     for(let i = 2; i < n; i++) {
-
-      fib[i] = fib[i-1] + fib[i-2]; 
       
+      fib[i]= fib[i-1] + fib[i-2]; 
     }
     return fib;
 }
