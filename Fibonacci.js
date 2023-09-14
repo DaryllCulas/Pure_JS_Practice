5fc8581cbc61bf22e8ee8f849b13bb7e76be3435
DaryllCulas <culasdaryll99@gmail.com>
2023-09-15 01:27:09 +0800

    Add Fibonacci.js

---
+++ Fibonacci.js
@@ -0,0 +1,13 @@
+function fibonacciSequence(n) {
+  const fib = [0,1];
+    for(let i = 2; i < n; i++) {
+
+      fib[i] = fib[i-1] + fib[i-2]; 
+      
+    }
+    return fib;
+}
+
+console.log(fibonacciSequence(2));
+console.log(fibonacciSequence(3));
+console.log(fibonacciSequence(7));
