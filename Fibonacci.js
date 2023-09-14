f781dd7d58888a0ab58544d2d0f0d690889e3e52
DaryllCulas <culasdaryll99@gmail.com>
2023-09-15 01:51:24 +0800

    Add Fibonacci.js

--- Fibonacci.js
+++ Fibonacci.js
@@ -3,6 +3,7 @@ function fibonacciSequence(n) {
     for(let i = 2; i < n; i++) {
       
       fib[i]= fib[i-1] + fib[i-2]; 
+      debugger;
     }
     return fib;
 }
@@ -10,3 +11,5 @@ function fibonacciSequence(n) {
 console.log(fibonacciSequence(2));
 console.log(fibonacciSequence(3));
 console.log(fibonacciSequence(7));
+
+//BIG-O => 0(n) = since the function has one loop occurs within
