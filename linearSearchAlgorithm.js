617815d2c4be215b23a9baf08680012b17d23b91
DaryllCulas <culasdaryll99@gmail.com>
2023-09-17 01:41:20 +0800

    Add linearSearchAlgorithm.js

--- linearSearchAlgorithm.js
+++ linearSearchAlgorithm.js
@@ -11,4 +11,4 @@ console.log(linearSearchAlgorithm([-5,2,10,4,6], 10));
 console.log(linearSearchAlgorithm([-5,2,10,4,6], 6));
 console.log(linearSearchAlgorithm([-5,2,10,4,6], 20));
 
-//Big-O => 0(n);
+//Big-O => 0(n); 
