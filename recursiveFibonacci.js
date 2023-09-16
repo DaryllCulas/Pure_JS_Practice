d08bc0b76172cce622d7b1899417714aaddb7662
DaryllCulas <culasdaryll99@gmail.com>
2023-09-16 23:37:26 +0800

    Add recursiveFibonacci.js

---
+++ recursiveFibonacci.js
@@ -0,0 +1,17 @@
+const recursiveFibonacci = (n) => {
+  const bracket = [];
+  if(n < 2) {
+    return n;
+  } else { 
+    return recursiveFibonacci(n-1) + recursiveFibonacci(n-2); 
+  }
+
+
+}
+
+console.log(recursiveFibonacci(0));
+console.log(recursiveFibonacci(1));
+console.log(recursiveFibonacci(6));
+
+// Big - O => O(n^2) -> recursive
+// Big - O => if iterative
