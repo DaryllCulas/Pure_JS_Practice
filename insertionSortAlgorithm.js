6754cdd59b475844185edb8c47d05ab57fd84f41
DaryllCulas <culasdaryll99@gmail.com>
2023-09-21 00:50:29 +0800

    Add insertionSortAlgorithm.js

---
+++ insertionSortAlgorithm.js
@@ -0,0 +1,17 @@
+const insertionSortAlgorithm = (arr) => {
+  for(let i = 1; i < arr.length; i++) {
+    let numberToInsert = arr[i];
+    let j = i - 1;
+    while(j >= 0 && arr[j] > numberToInsert) {
+      arr[j+1] = arr[j];
+        j = j-1;
+    }
+    arr[j+1] = numberToInsert;
+  }
+}
+
+
+const arr = [8, 20, -2, 4, -6];
+insertionSortAlgorithm(arr);
+console.log(arr);
+//Big-O => O(n^2) -> Quadratic time complexity (2 loops)
