5757e7ea4473fc77ffcc62e2359e4462c0a5c862
DaryllCulas <culasdaryll99@gmail.com>
2023-09-21 23:36:11 +0800

    Add WorkingWithVariables.js

---
+++ JSinfo/Javascript Fundamentals/WorkingWithVariables.js
@@ -0,0 +1,14 @@
+// Task 1
+let name = "John"; 
+let admin = name;
+
+
+console.log(admin);
+
+
+// Task 2
+
+let planet = "Earth"
+let user = "Daryll"
+
+console.log(`Hello ${user}, you are currently living in this ${planet} `);
