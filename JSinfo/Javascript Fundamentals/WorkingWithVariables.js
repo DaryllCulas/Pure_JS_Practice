b487c588297079e9d849e0b8bce7428612fff176
DaryllCulas <culasdaryll99@gmail.com>
2023-09-21 23:38:42 +0800

    Add WorkingWithVariables.js

--- JSinfo/Javascript Fundamentals/WorkingWithVariables.js
+++ JSinfo/Javascript Fundamentals/WorkingWithVariables.js
@@ -12,3 +12,14 @@ let planet = "Earth"
 let user = "Daryll"
 
 console.log(`Hello ${user}, you are currently living in this ${planet} `);
+
+
+// Task 3
+function someCode(bday) {
+  return bday;
+}
+const birthday = "05.17.1999";
+
+const age = someCode(birthday);
+
+console.log(age);
